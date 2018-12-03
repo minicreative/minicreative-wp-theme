1e8426dfdd3c957b137907ce77684032a728b9f9
Tomas Roy <hello@minicreative.net>
2018-12-03 11:57:47 -0600

    gulpfile destination path fixed

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
-var env = require("environment.json");
+var env = require("./environment.json");
 var gulp = require("gulp");
 
 // Structure: copies files to the home directory for CMS functionality
 gulp.task("structure", function () {
 	if (env.cms == "wordpress")
-		gulp.src("wp_base/*.*").pipe(gulp.dest("/"));
+		gulp.src("wp_base/*.*").pipe(gulp.dest("."));
 	if (env.cms == "processwire")
-		gulp.src("pw_base/*.*").pipe(gulp.dest("/"));
+		gulp.src("pw_base/*.*").pipe(gulp.dest("."));
 })
